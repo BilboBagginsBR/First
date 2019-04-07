718790129659f9a85a416674168a8192b118444d
Dmitry <dmitrijharitonov408@gmail.com>
2019-04-07 22:41:08 +0500

    Second commit

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 " use strict";
 
-var money = +prompt("Your selary a month: ", ""),
-	time = prompt("Date YYYY-MM-DD: ", ""),
-	stateOfExpense = prompt("Urgent expense: ", ""),
-	priceOfExpense = +prompt("Price of expense: ", "");
-var appData = {
-	Budget: money,
-	Time: time,
-	Expenses: {
-		stateOfExpense: priceOfExpense
-	},
-	optinalExpenses: {},
-	income: {},
-	savings: false
+let money = +prompt("Your selary a month: ", ""),
+    time = prompt("Date YYYY-MM-DD: ", "");
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optinalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
 };
 
-alert(money/30);
+let i = 0;
+while (i < 2) {
+    let a = prompt("Urgent expense: ", ""),
+        b = prompt("Price of expense: ", "");
+
+    if ((typeof (a) === 'string') && (a != null) && (b != null) &&
+        (a != '') && (b != '') && (a.length < 50)) {
+        console.log("Done");
+        appData.expenses[a] = b;
+
+    }
+    i++;
+
+}
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Urgent expense: ", ""),
+//         b = prompt("Price of expense: ", "");
+
+//     if ((typeof (a) === 'string') && (a != null) && (b != null) &&
+//         (a != '') && (b != '') && (a.length < 50)) {
+//         console.log("Done");
+//         appData.expenses[a] = b;
+
+//     }
+// }
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Every day budget: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Min level of life");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Mid level of life");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High level of life")
+} else {
+    console.log("Error")
+}
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,1 @@
-function fact(num){
-	if (num==1) return 1
-		else return num*fact(num-1);
-}
-
-console.log(fact(11));
+console.log([ ] + 1 + 2)
